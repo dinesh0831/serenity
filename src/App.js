53e9145bc0b619f897d434a225bd1e0a0cda5f90
Dinesh <81325951+dinesh0831@users.noreply.github.com>
2022-10-05 17:26:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import AvailableVacation from './home';
 import { BrowserRouter, Switch, Route, } from "react-router-dom";
